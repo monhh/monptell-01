b4f7045d2817a8905991b80e2ca17644fd40300f
mon.ptell <mon.medialab@gmail.com>
2025-02-18 04:15:57 +0100

    feat: deshabilitar viewport when open menu

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const menuIcon = document.querySelector(".menu-icon");
 const navbar = document.querySelector(".navbar");
 
+
 /* add or remove the "change" class from the "nav" */
 menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change")
+    navbar.classList.toggle("change");
+    /* deshabilitando el desplazamiento vertical del viewport when open menu */
+    document.body.classList.toggle("menu-open");
+
 })
+

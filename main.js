113b76a1c5f9443f695a3cc817c0d279922fcf1d
mon.ptell <mon.medialab@gmail.com>
2025-03-19 01:01:19 +0100

    feat: split CSS and BEM

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const menuIcon = document.querySelector(".menu-icon");
+const menuIcon = document.querySelector(".header__menu-icon");
 const navbar = document.querySelector(".navbar");
 
 
 /* add or remove the "change" class from the "nav" */
 menuIcon.addEventListener("click", () => {
-    navbar.classList.toggle("change");
+    navbar.classList.toggle("overlay-nav--active");
     /* deshabilitando el desplazamiento vertical del viewport when open menu */
     document.body.classList.toggle("menu-open");
 

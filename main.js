554c4b00f62067322c940817621fd3fef5d1d34d
mon.ptell <mon.medialab@gmail.com>
2025-05-11 01:36:30 +0200

    fusion .header 2

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const menuIcon = document.querySelector(".header__menu-icon");
+const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
 
 
 /* add or remove the "change" class from the "nav" */
-menuIcon.addEventListener("click", () => {
+hamburger.addEventListener("click", () => {
     navbar.classList.toggle("overlay-nav--active");
     /* deshabilitando el desplazamiento vertical del viewport when open menu */
     document.body.classList.toggle("menu-open");

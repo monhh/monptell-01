ef9fd7412e006f36c6aa867ff304843d1796f48f
mon.ptell <mon.medialab@gmail.com>
2025-05-11 03:17:13 +0200

    fusion overlay

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const navbar = document.querySelector(".navbar");
 
 /* add or remove the "change" class from the "nav" */
 hamburger.addEventListener("click", () => {
-    navbar.classList.toggle("overlay-nav--active");
+    navbar.classList.toggle("active");
     /* deshabilitando el desplazamiento vertical del viewport when open menu */
     document.body.classList.toggle("menu-open");
 

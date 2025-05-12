be59da756eec289636102b4e98837872d566be0e
mon.ptell <mon.medialab@gmail.com>
2025-05-12 23:37:40 +0200

    deshabilitar la clase menu-open al clikear en logo y fusionar all js

--- filter.js
+++ filter.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".card");
-    const logo = document.querySelector("a.logo"); // Selecciona logo
 
-    // Función para filtrar
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".navbar");
+
+    /* Toggle del menu hamburger */
+    hamburger.addEventListener("click", () => {
+        navbar.classList.toggle("active");
+        /* deshabilitando el desplazamiento vertical del viewport when open menu */
+        document.body.classList.toggle("menu-open");
+    });
+
+    
+    // Filtrar cards según la categoría
+    const cards = document.querySelectorAll(".card");
+
+    // función para filtrar las cards
     const filterCards = (category) => {
         cards.forEach((card) => {
             const cardCategory = card
@@ -16,15 +28,12 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     };
 
-    // Evento click en los enlaces de categoría
+    // click en las cards (categorías)
     document.querySelectorAll(".category a").forEach((link) => {
         link.addEventListener("click", (e) => {
             e.preventDefault(); // Evita la navegación
             const category = link.textContent.trim().toLowerCase();
             filterCards(category);
-
-            // Opcional: Actualizar la URL sin recargar
-            /* history.pushState({}, "", `?category=${category}`); */
 
             // Al filtrar guardamos un estado en el historial
             history.pushState(
@@ -35,7 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    // Click en el logo - Restaurar estado inicial
+
+    // Restaurar estado inicial al clickear el logo y cerrar el menú
+    const logo = document.querySelector("a.logo");
+
     logo.addEventListener("click", (e) => {
         e.preventDefault(); // Importante para evitar recarga
         filterCards("all"); // Muestra todas las cards
@@ -46,18 +58,20 @@ document.addEventListener("DOMContentLoaded", () => {
         if (navbar) {
             navbar.classList.remove("active");
         }
+        /* deshabilitar la clase "menu-open" */
+        document.body.classList.remove("menu-open");
     });
 
-    // Manejar botón de retroceso
+
+    // Manejar botón de retroceso del navegador
     window.addEventListener("popstate", (e) => {
-        // Si venimos de un filtrado, restauramos a 'all'
         // Si el estado es "filtered", restaurar todas las cards
         filterCards(e.state?.category === "filtered" ? "all" : "all");
     });
 
-    // Filtrado desde el menú de navegación
-    //Selección de enlaces del menú:
-    // Selecciona todos los links excepto el que tiene clase 'link' y 'no'
+
+    // Filtrado con menú de navegación (overlay) y su cierre
+        // Selecciona todos los links excepto el que tiene clase 'link' y 'no'
     document.querySelectorAll(".overlay a:not(.link.no)").forEach((navLink) => {
         navLink.addEventListener("click", (e) => {
             e.preventDefault();
@@ -68,11 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
             const navbar = document.querySelector(".navbar.active"); // Asegúrate de que este selector coincida con tu estructura real
             if (navbar) {
                 navbar.classList.remove("active");
-                // selecciona el body y se le quita la clase "menu-open"
-                document.body.classList.toggle("menu-open");
             }
-            
         });
     });
-
 });
